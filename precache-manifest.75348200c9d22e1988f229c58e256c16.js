5696d57c9ee6772961caf3a297e23822bf14a7b5
gezaramdaciptaan <gezaramdaciptaan@gmail.com>
2020-04-15 15:24:25 +0700

    Updates

---
+++ precache-manifest.75348200c9d22e1988f229c58e256c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e155a5066198f01b8214db1b636472",
+    "url": "/2020_UTS_Praktikum_Web3/index.html"
+  },
+  {
+    "revision": "a805eb6ac20c48000bb5",
+    "url": "/2020_UTS_Praktikum_Web3/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c66aaca343be1128a181",
+    "url": "/2020_UTS_Praktikum_Web3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a805eb6ac20c48000bb5",
+    "url": "/2020_UTS_Praktikum_Web3/static/js/2.2c5c46bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2020_UTS_Praktikum_Web3/static/js/2.2c5c46bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66aaca343be1128a181",
+    "url": "/2020_UTS_Praktikum_Web3/static/js/main.a013e449.chunk.js"
+  },
+  {
+    "revision": "588c9372599b9e933c60",
+    "url": "/2020_UTS_Praktikum_Web3/static/js/runtime-main.232dbab8.js"
+  }
+]);
